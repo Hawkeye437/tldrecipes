5ead3d898e6cb7c375c198530f108015d490ee55
Cassio Sousa <cassiosantossousa@gmail.com>
2014-11-09 17:59:04 -0600

    Minor changes to New User page

--- cassio/header.js
+++ cassio/header.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('title').append(' | TL;DRecipes');
     $('head').append('<!-- Font loading -->');
-    $('head').append('<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" rel="stylesheet" type="text/css">');
+    $('head').append('<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">');
     $('head').append('<!-- favicon image -->');
     $('head').append('<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">');
     $('head').append('<link rel="icon" href="../img/favicon.ico" type="image/x-icon">');
