9428bf4ef15d36c06d35f89faf27cc98c817ced8
Cassio Sousa <cassiosantossousa@gmail.com>
2014-11-09 16:58:19 -0600

    Sent Title and Favicon to Header.js

---
+++ cassio/header.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('title').append(' | TL;DRecipes');
+    $('head').append('<!-- favicon image -->');
+    $('head').append('<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">');
+    $('head').append('<link rel="icon" href="../img/favicon.ico" type="image/x-icon">');
+});
