28b0a312b1117c09bac76d259f004106ff3c2273
allenmyao <alleyao@gmail.com>
2014-11-09 19:21:56 -0600

    Added recipe card diet hover text, randomized diet icons

--- allen/card.js
+++ allen/card.js
@@ -4,8 +4,20 @@ $(document).ready(function() {
 	loadCards($('.recipes'));
 	$('body').on('DOMNodeInserted', function(event) {
 		if ($(event.target).is('.recipes')) {
-		loadCards($(event.target));
-	}
+			loadCards($(event.target));
+		}
+	});
+
+	$('.diet-icon').on('mouseenter', function(event) {
+		var tooltip = $(this).siblings('.diet-tooltip');
+		var tooltipText = $(this).data('tooltip');
+		tooltip.text(tooltipText);
+		tooltip.show();
+	});
+	$('.diet-icon').on('mouseout', function(event) {
+		var tooltip = $(this).siblings('.diet-tooltip');
+		tooltip.text('');
+		tooltip.hide();
 	});
 });
 
@@ -14,11 +26,34 @@ function loadCards(listings) {
 		$(this).html('<div class="recipe-listing-wrapper"><div class="recipe-listing-inner-wrapper"><ul class="recipe-listing"></ul></div></div>');
 		var listing = $(this).find('.recipe-listing');
 		var numCards = $(this).data('length');
+		var diets = [
+			'<a href="../allen/browse.html?category=Vegetarian Recipes" class="diet-icon vegetarian" data-tooltip="Vegetarian">V</a>',
+			'<a href="../allen/browse.html?category=Vegan Recipes" class="diet-icon vegan" data-tooltip="Vegan">Vg</a>',
+			'<a href="../allen/browse.html?category=Dairy-Free Recipes" class="diet-icon dairy" data-tooltip="Dairy-Free">Df</a>',
+			'<a href="../allen/browse.html?category=Kosher Recipes" class="diet-icon kosher" data-tooltip="Kosher">K</a>',
+			'<a href="../allen/browse.html?category=Gluten-Free Recipes" class="diet-icon gluten" data-tooltip="Gluten-Free">Gl</a>'
+		];
 		for (var i = 0; i < numCards; i++) {
 			var max = 100;
 			var min = 1;
 			var randSuccessRate = Math.floor(Math.random() * (max - min + 1) + min);
-			listing.append('<li class="recipe-card"><a href="../will/recipe.html"><div class="recipe-card-img" style="background-image: url(\'../allen/card.jpg\')"></div></a><div class="recipe-card-info"><div class="recipe-info-top-wrapper"><div class="recipe-info-name-wrapper"><div class="recipe-name"><a href="../will/recipe.html">Pasta</a></div><div class="recipe-user">by <a href="../cassio/userpage.html">pastafreak123</a></div></div><div class="recipe-diet"><div class="diet-icon vegetarian">V</div><div class="diet-icon vegan">Vg</div><div class="diet-icon dairy">Df</div><div class="diet-icon kosher">K</div><div class="diet-icon gluten">Gl</div></div></div><div class="recipe-success"><div class="success-bar"><div class="success" style="width: ' + randSuccessRate + '%"></div></div><div class="success-text"><span class="success-rate">' + randSuccessRate + '%</span> success rate</div></div><div class="recipe-time"><span>Time: <span>1 hr</span> &amp; <span>30 min</span></span></div></div></li>');
+			shuffle(diets);
+			var card = '<li class="recipe-card"><a href="../will/recipe.html"><div class="recipe-card-img" style="background-image: url(\'../allen/card.jpg\')"></div></a><div class="recipe-card-info"><div class="recipe-info-top-wrapper"><div class="recipe-info-name-wrapper"><div class="recipe-name"><a href="../will/recipe.html">Pasta</a></div><div class="recipe-user">by <a href="../cassio/userpage.html">pastafreak123</a></div></div><div class="recipe-diet">'
+					+ diets[0] + diets[1] + diets[2]
+					+ '<div class="diet-tooltip"></div></div></div><div class="recipe-success"><div class="success-bar"><div class="success" style="width: '
+					+ randSuccessRate + '%"></div></div><div class="success-text"><span class="success-rate">'
+					+ randSuccessRate + '%</span> success rate</div></div><div class="recipe-time"><span>Time: <span>1 hr</span> &amp; <span>30 min</span></span></div></div></li>';
+			listing.append(card);
 		}
 	});
 };
+
+function shuffle(a,b,c,d) {
+	c = a.length;
+	while (c) {
+		b = Math.random() * c-- | 0;
+		d = a[c];
+		a[c] = a[b];
+		a[b]=d;
+	}
+}
