83d96006249bfc2cab92f7e190bb1eb6d52e5b78
Cassio Sousa <cassiosantossousa@gmail.com>
2014-11-09 18:57:40 -0600

    Minor changes to the content

--- cassio/header.js
+++ cassio/header.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('title').append(' | TL;DRecipes');
+    $('title').append(' | TL&#59;DRecipes');
     $('head').append('<!-- Font loading -->');
     $('head').append('<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">');
-    $('head').append('<!-- favicon image -->');
+    $('head').append('<!-- Favicon image -->');
     $('head').append('<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">');
     $('head').append('<link rel="icon" href="../img/favicon.ico" type="image/x-icon">');
 });
