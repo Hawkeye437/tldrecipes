ccfdc5b93cb3cd60ec3f812d4c70934f2230e12a
William Xie <wxie6@illinois.edu>
2014-11-09 23:55:31 -0600

    Submit2 finished

--- will/submit.js
+++ will/submit.js
@@ -1,2 +1,8 @@
 /* submit.js */
 
+// bad practice but temporary for prototype
+var HTML_INGROW = '<div class="ing-row"><div class="ing-cell"><input class="ing-num form-control" type="text" maxlength="2"></div><div class="ing-cell"><input class="ing-name form-control" type="text"></div></div>';
+
+$("#ingAdd").click(function() {
+	$(".ing-wr").append(HTML_INGROW);
+});
