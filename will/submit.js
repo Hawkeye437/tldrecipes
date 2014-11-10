07db0cc34d564f365a3d0d217bac53bf0eadc4d7
William Xie <wxie6@illinois.edu>
2014-11-10 00:27:47 -0600

    Finished submit recipes pages for the prototype demo

--- will/submit.js
+++ will/submit.js
@@ -2,7 +2,16 @@
 
 // bad practice but temporary for prototype
 var HTML_INGROW = '<div class="ing-row"><div class="ing-cell"><input class="ing-num form-control" type="text" maxlength="2"></div><div class="ing-cell"><input class="ing-name form-control" type="text"></div></div>';
+var HTML_STEPROW1 = '<div class="ing-row"><div class="ing-cell"><span class="step-icon">';
+var HTML_STEPROW2 = '</span></div><div class="ing-cell"><textarea class="ing-name step-cell form-control" type="textarea" rows="3"></textarea></div></div>';
+
+var num = 3;
 
 $("#ingAdd").click(function() {
 	$(".ing-wr").append(HTML_INGROW);
+});
+
+$("#stepAdd").click(function() {
+	num++;
+	$(".ing-wr").append(HTML_STEPROW1 + num + HTML_STEPROW2);
 });
