0117dd352e12c9ad3a3ad7fc5a76643093b45e75
allenmyao <alleyao@gmail.com>
2014-11-10 21:12:52 -0600

    Updated header favicon path

--- js/header.js
+++ js/header.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     $('head').append('<!-- Font loading -->');
     $('head').append('<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">');
     $('head').append('<!-- Favicon image -->');
-    $('head').append('<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">');
-    $('head').append('<link rel="icon" href="../img/favicon.ico" type="image/x-icon">');
+    $('head').append('<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">');
+    $('head').append('<link rel="icon" href="img/favicon.ico" type="image/x-icon">');
 });
