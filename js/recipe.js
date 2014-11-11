aa980ae46a8db41bc697651d7c9b0be4b320660e
William Xie <wxie6@illinois.edu>
2014-11-10 23:22:14 -0600

    Recipe page header, picture, and ingredients list

--- js/recipe.js
+++ js/recipe.js
@@ -1,1 +1,33 @@
 //
+
+var ingrList = ['Drop of African Children Tears', 'Green Onion', 'Cup of Cheese'];
+var ctr = 0;
+
+// First time
+$(".swap").click(function() {
+	var row = $(".swap").parent()
+	console.log(row);
+	row.fadeOut(300, function() {
+		row.empty();
+		ctr++;
+		if (ctr > 2) {
+			ctr = 0;
+		}
+		row.append('<div class="ingr-cell ingr-num">1</div><div class="ingr-cell">' + ingrList[ctr] + '</div><div class="ingr-cell swap" onclick="swapIngr(this)"><i class="fa fa-refresh"></i></div>').fadeIn(300);
+	});
+});	
+
+// After first time
+function swapIngr(e) {
+	var self = e;
+	var row = $(e).parent();
+	console.log(row);
+	row.fadeOut(300, function() {
+		row.empty();
+		ctr++;
+		if (ctr > 2) {
+			ctr = 0;
+		}
+		row.append('<div class="ingr-cell ingr-num">1</div><div class="ingr-cell">' + ingrList[ctr] + '</div><div class="ingr-cell swap" onclick="swapIngr(this)"><i class="fa fa-refresh"></i></div>').fadeIn(300);
+	});
+}
