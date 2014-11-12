cc927686a711d71c0b83e037adaddf92dd39c9d9
allenmyao <alleyao@gmail.com>
2014-11-12 01:02:44 -0600

    Added filter to search results page

--- js/search-results.js
+++ js/search-results.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 	var type = location.search.split('type=')[1]
 	if (type) {
-		type = type.replace(/%20/g, ' ');
+		type = decodeURIComponent(type);
 		if (type == 'users')
-			$('.section').append('<div class="users" data-length="12"></div>');
+			displayUsers();
 		else
-			$('.section').append('<div class="recipes" data-length="12"></div>');
+			displayRecipes();
 	} else {
-		$('.section').append('<div class="recipes" data-length="12"></div>');
+		displayRecipes();
 	}
+
 
 	$(window).scroll(function() {
 		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
@@ -20,3 +21,15 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function displayRecipes() {
+	$('.section').append('<div class="recipes" data-length="12"></div>');
+	$('.search-filter .recipe-filter').show();
+	$('.search-filter .user-filter').hide();
+}
+
+function displayUsers() {
+	$('.section').append('<div class="users" data-length="12"></div>');
+	$('.search-filter .user-filter').show();
+	$('.search-filter .recipe-filter').hide();
+}
